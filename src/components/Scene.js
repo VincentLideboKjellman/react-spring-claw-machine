8a981aa8d6af18d26678d286f6c100e5b577b374
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2020-11-25 17:44:20 +0100

    Feat: Add repeated animation

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -16,45 +16,61 @@ const StyledScene = styled(animated.div)`
     width: ${props => (props.scenewidth ? props.scenewidth : "auto")};
     height: ${props => (props.sceneweight ? props.sceneweight : "auto")};
     background-color: #000;
-    
 `
 
 const Scene = ({...props}) => {
+
+  let [isPressed, setPressed] = useState(false);
   
   const fade = useSpring({
     from: {
-      opacity: 0
+      opacity: 0,
     },
     to: {
-      opacity: 1
+      opacity: 1,
     }
   });
 
-  const toRight = useSpring({
-    from: {
-      opacity: 0
+  //Back and forth moving animation
+  const repeat = useSpring({
+    from: { 
+      transform: 'translate3d(0%,0,0)'
+     },
+    to: async next => {
+      while (!isPressed) {
+        await next({ transform: 'translate3d(20%,0,0)' })
+        await next({ transform: 'translate3d(0%,0,0)' })
+        await next({ transform: 'translate3d(-20%,0,0)' })
+        await next({ transform: 'translate3d(0%,0,0)' })
+      }
     },
-    to: {
-      opacity: 1
-    }
-  });
+    config: {
+      mass: 100, tension: 580, friction: 200,
+    },
+  })
 
+
+console.log(isPressed);
 
 
   return (
+    <>
       <StyledScene style={fade} {...props}>
         <Backdrop backWidth={props.scenewidth} backHeight={props.sceneweight} />
         <PizzaPile/>
         <Table/>
         {/* Claw */}
-        <div className="claw-machine">
-          <ClawBase />
-          <div className="claws">
+        <animated.div style={repeat} className="claw-machine">
+          <ClawBase/>
+          <animated.div className="claws">
             <ClawLeft />
             <ClawRight />
-          </div>
-        </div>
+          </animated.div>
+        </animated.div>
       </StyledScene>
+        <button style={{position:"absolute", top:0, color: "red", zIndex: "20"}} onClick={() => setPressed(isPressed = true)}>OFF</button>
+        <button style={{position:"absolute", top:20, color: "green", zIndex: "20"}} onClick={() => setPressed(isPressed = false)}>ON</button>
+      </>
   );
 }
 
